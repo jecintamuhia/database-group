3e899fa9ac62c9da070f24fd86c25d47d423ccc3
Behind Seen <jecintawanjirumuhia@gmail.com>
2025-05-12 20:46:28 +0300

    inserted data in the parents table

---
+++ tables.sql
@@ -0,0 +1,13 @@
+INSERT INTO vaccination_data.parents(parentid,parentname,gender,phonenumber,identificationnumber)VALUES
+(004,'janet meena','female','+254765289','45368290'),
+(006,'milicent odhiambo','female','+254765438','65473892'),
+(007,'nelson mandela','male','+254768590','67499322'),
+(009,'becky willy','female','+254754325','65437822'),
+(010,'immaculate','female','+254765899','45271891'),
+(011,'joan bongo','female','+254754342','23456778'),
+(012,'joseph juma','male','+254677888','56377780'),
+(013,'hassan joel','male','+254754256','24536797'),
+(014,'rajab maulid','male','+254723454','65378294'),
+(015,'eliza beth','female','+254754355','66738929')
+
+SELECT * FROM vaccination_data.parents
